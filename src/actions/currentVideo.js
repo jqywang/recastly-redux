596252c4e80f97ad9369cef41facbfb274936504
Jeffrey Wang <jqywang.95@gmail.com>
2018-04-02 16:32:12 -0700

    wrote two action creators

--- src/actions/currentVideo.js
+++ src/actions/currentVideo.js
@@ -1,5 +1,7 @@
 var changeVideo = (video) => ({
   //TODO:  Return some action object to change the currently playing video.
+  type: "VIDEO_CHANGED",
+  newVid: video,
 });
 
 export default changeVideo;
